e4b9b06f7ea8e3ac7417ea4566716d1dd05a7f4c
mithun <mithunpaul08@gmail.com>
2019-06-12 16:38:08 -0700

    ancestors works as dictionary as output. but still need to get it as a list

--- main.py
+++ main.py
@@ -75,13 +75,26 @@ def ont_node(name, examples, keywords,appliesTo, add_name = True):
     return d
 
 #given any child/leaf node, get the complete path of it starting from the top most parent of it
-def make_ancestry(child_parent_dictionary, label, ancestors):
-    if label not in child_parent_dictionary:
-            return label
-    for c in child_parent_dictionary[label]:
-        n = make_ancestry(child_parent_dictionary,c, ancestors)
-        ancestors=ancestors+"/"+n
-    return ancestors
+
+def make_ancestry(parent_child_dict, label):
+    if label not in parent_child_dict:
+        return label
+    node = []
+    for c in parent_child_dict[label]:
+        n = make_ancestry(parent_child_dict, c)
+        n="".join(n)
+        node.append(n)
+    return node
+
+def make_ancestry_dict(parent_child_dict, label):
+    if label not in parent_child_dict:
+        return label
+    node = {label:[]}
+    for c in parent_child_dict[label]:
+        n = make_ancestry(parent_child_dict, c)
+        node[label].append(n)
+    return node
+
 
 def make_hierarchy(parent_child_dict, label):
     if label not in parent_child_dict:
@@ -107,8 +120,7 @@ if __name__ == '__main__':
     g.load('data/rdx/root-ontology.owl')
     event_obj_for_appliedTo=get_obj_event_appliedTo_sparql(g)
     child_parent_dict, parent_child_dict=get_parent_child_sparql(g)
-    ancestors=make_ancestry(child_parent_dict,"Footwear","")
-
+    ancestors=make_ancestry(child_parent_dict,"Footwear")
     data = [
         make_hierarchy(parent_child_dict, 'Events'),
         make_hierarchy(parent_child_dict, 'Objects'),

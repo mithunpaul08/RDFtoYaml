f00debce398631307f617ec3dcc254352192421e
mithun <mithunpaul08@gmail.com>
2019-06-25 15:04:59 -0700

    marco rewrote code same function more nicer

--- read_yaml.py
+++ read_yaml.py
@@ -26,7 +26,6 @@ def get_ancestry_tree(child_parent_dict, label):
     return n
 
 names=[]
-#go through each of the nodes in yaml dictionary. Note down all values of the "name" key into a list
 def parse_yaml_get_all_names(k,v):
     if(k=="name"):
         names.append(v)
@@ -46,12 +45,37 @@ def parse_yaml_get_all_names(k,v):
                                             parse_yaml_get_all_names(kchild,vchild)
 
 
+
+
+def find_all_names(data):
+    if type(data) == list:
+        names = []
+        for elem in data:
+            names.extend(find_all_names(elem))
+        return names
+    if type(data) == dict:
+        # am i on a leaf?
+        if 'OntologyNode' in data:
+            # yes, i am in a leaf
+            if 'name' not in data:
+                # all leaves should have name
+                raise Exception('invalid ontology node')
+            return [data['name']]
+        else:
+            # no, i am not in a leaf
+            for v in data.values():
+                return find_all_names(v)
+
+
 stream = open('converted_file.yml', 'r')
 ont_dict=yaml.load(stream)
-for key,value in (ont_dict[0].items()):
-    parse_yaml_get_all_names(key,value)
-print(f"list of name nodes is{names}")
 
+names = find_all_names(ont_dict)
+
+#go through each of the nodes in yaml dictionary. Note down all values of the "name" key into a list
+# for key,value in (ont_dict[0].items()):
+#     parse_yaml_get_all_names(key,value)
+print(f"list of name nodes is{names}")
 
 #for each of these values, get its full path
 
